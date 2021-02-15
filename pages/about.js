a1c0cb9a6d6f052a02ac6f407fd9ac5ca1ac41e6
Claire2788 <51021233+Claire2788@users.noreply.github.com>
2021-02-15 15:03:06 +0200

    Next App

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+//Import the Link API
+import Link from 'next/Link'
+export default() => (
+ <div>
+    <Link href="/index">
+    <a>Home</a>
+    </Link>
+        <p>Claire Smithard</p>
+        <p>I am a Digital Marketer with Front End Development skill set. I am responsible for managing our brands websites, social media, SEO and paid advertising. Coupled with this I am a keen problem solver, delving into our business and providing custom developed solutions for our teams. </p>
+ </div>
+)
